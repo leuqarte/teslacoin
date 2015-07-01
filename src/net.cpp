3e2d3e3ddbff9b42fc2e841b7476b31685a546d8
Valkir <valkircrypto@gmail.com>
2015-06-30 20:30:53 -0400

    Fixing Seeds

--- src/net.cpp
+++ src/net.cpp
@@ -1149,7 +1149,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"tesla.coin1", "seed1.teslachain.info"},
+    {"tesla.coin1", "tes1.altcoinsfoundation.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
